db63b7a931cea3cdf84d4521a9c34a42d20a2bf0
SlickJacket <novenarii@gmail.com>
2019-12-05 01:07:18 -0500

    completed gradebook

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -52,7 +52,21 @@ namespace GradeBook
 
         public override Statistics GetStatistics()
         {
-            throw new NotImplementedException();
+            var result = new Statistics();
+
+            using (var reader = File.OpenText($"{Name}.txt"))
+            {
+                var line = reader.ReadLine();
+                
+                while (line != null)
+                {
+                    var number = double.Parse(line);
+                    result.Add(number);
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
         }
     }
 
@@ -112,40 +126,12 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.Low = double.MaxValue;
-            result.High = double.MinValue;
 
             foreach (var grade in grades)
             {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
+                result.Add(grade);
+                
             }
-
-            result.Average /= grades.Count;
-
-            switch (result.Average)
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-                case var d when d >= 50.0:
-                    result.Letter = 'F';
-                    break;
-                default:
-                    break;
-            }
-
             return result;
         }
 
