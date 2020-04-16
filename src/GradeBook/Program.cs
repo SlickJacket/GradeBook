dfe87f3f2b546ecb43d5380af759463594dcde9c
SlickJacket <novenarii@gmail.com>
2020-04-16 11:51:39 -0400

    initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 43.2;
+            var y = 47.6;
+            var added = x + y;
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}! You have {added} apples.");
+            }
+            else {
+                Console.WriteLine("Goodbye");
+            }
+        }
+    }
+}
