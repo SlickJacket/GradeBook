0d3ef6e6e360ba3f80f8dae1cb2978326c0fa883
SlickJacket <novenarii@gmail.com>
2020-04-16 12:28:38 -0400

    defined average of grades List

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -10,11 +11,27 @@ namespace GradeBook
             var y = 47.6;
             var added = x + y;
 
+            // initializing an array
+            var numbers = new[] { 24.2, 12.1, 21.3, 14.1 };
+            // initializings a list
+            var grades = new List<double>() { 24.2, 12.1, 21.3, 14.1 };
+            grades.Add(34.5);
+            var result = 0.0;
+
+            foreach(var num in grades){
+                result += num;
+            }
+
+            var average = result / grades.Count;
+            System.Console.WriteLine(result);
+            System.Console.WriteLine(average);
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]}! You have {added} apples.");
             }
-            else {
+            else
+            {
                 Console.WriteLine("Goodbye");
             }
         }
